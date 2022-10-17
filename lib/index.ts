8c948de2f4c09a5a0652d03c7a575863f8c3cd4e
Cody Dalton <cody.dalton@cerifi.com>
2022-10-17 11:38:12 -0400

    Update decorator locations

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,5 @@
 export * from './compiler/decorators/module.decorator';
 export * from './compiler/decorators/component.decorator';
 export * from './compiler/decorators/service.decorator';
+export * from './http/';
+export * from './http/decorators/mapping.decorator';
